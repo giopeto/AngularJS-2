91ca275de96697740e89cc934497352159538bca
root <georg3georgiev@gmail.com>
2016-01-12 14:58:52 +0200

    Added angular2 form example

--- angular2-quickstart/app/app.component.ts
+++ angular2-quickstart/app/app.component.ts
@@ -2,6 +2,6 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1> 32423423dadsadsa  2112321 </h1>'
+    template: '<h1> Angular.io quickstart </h1>'
 })
 export class AppComponent { }
